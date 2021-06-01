c8eb5ddbfdab39e73023fb16e64d39ccbe0682ec
Ian Christopher Masbang <ianmasbang77@gmail.com>
2021-06-01 22:09:07 +0800

    add scripts

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 5000;
+}
+app.listen(port, ()=>{
+    console.log('Server started on port ' + port);
+});
+
+app.get('/', (req, res)=>{
+    res.send('API running');
+
+});
